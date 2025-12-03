6e4255b97c4ed69530f70bb0b338b4080b35a51e
Tom Pennington <tompennington.92@gmail.com>
2025-12-03 23:08:27 +0000

    remove dist folder

--- dist/src/lambdas/authorizer/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { APIGatewayRequestAuthorizerEvent, APIGatewayAuthorizerResult, Context } from 'aws-lambda';
-export declare const handler: (event: APIGatewayRequestAuthorizerEvent, _context: Context) => Promise<APIGatewayAuthorizerResult>;
--- dist/src/lambdas/health/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { APIGatewayProxyResultV2 } from 'aws-lambda';
-export declare const handler: () => Promise<APIGatewayProxyResultV2>;
