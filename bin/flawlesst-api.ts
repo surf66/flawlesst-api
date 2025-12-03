f63fa7653b44f00507500809a82efd541c39618b
Tom Pennington <tompennington.92@gmail.com>
2025-12-03 22:34:01 +0000

    Fix pipeline

--- bin/flawlesst-api.ts
+++ bin/flawlesst-api.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { FlawlesstApiStack } from '../infrastructure/lib/flawlesst-api-stack';
+import { FlawlesstApiStack } from '../lib/flawlesst-api-stack';
 
 const app = new cdk.App();
 
